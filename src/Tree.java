8ea3e55b4e5731c76fe1119645f09f51e124b465
Edison Yao <edisonj.yao@mail.utoronto.ca>
2024-09-16 10:09:58 -0400

    Added tree attributes

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,24 @@
 public class Tree {
+    private Integer root;
+
+    private Tree left;
+    private Tree right;
+
+    public Tree(Integer root) {
+        if (root != null) { 
+            this.root = root;
+            left = new Tree();
+            right = new Tree();
+        }
+
+    }
+
+    /**
+     * Alternate constructor, so we don't have to explicitly pass in null.
+     */
+    public Tree() {
+        this(null);
+    }
+
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
 }
