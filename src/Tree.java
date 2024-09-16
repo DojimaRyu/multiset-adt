87bdece1f18a8df6a1b82c4ee1d7e8fb86fb8989
Edison Yao <edisonj.yao@mail.utoronto.ca>
2024-09-16 10:25:00 -0400

    Added tree methods

--- src/Tree.java
+++ src/Tree.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
+
 public class Tree {
     private Integer root;
 
-    private Tree left;
-    private Tree right;
+    private ArrayList<Tree> subtrees;
 
     public Tree(Integer root) {
         if (root != null) { 
@@ -19,6 +20,50 @@ public class Tree {
     public Tree() {
         this(null);
     }
+
+    public boolean isEmpty() {
+        if (this.root == null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public int len() {
+        if (this.isEmpty()) {
+            return 0;
+        }
+
+        else {
+            int size = 1;
+            for (Tree t : subtrees) {
+                size += t.len();
+            }
+
+            return size;
+
+        }
+    }
+
+    public int count(int item) {
+        if (this.isEmpty()) {
+            return 0;
+        }
+
+        else {
+            int num = 0;
+            if (this.root.equals(item)) {
+                num += 1;
+            }
+
+            for (Tree t : subtrees) {
+                num += t.count(item);
+            }
+
+            return num;
+        }
+    }
+
 
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
 }
